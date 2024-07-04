c335b800f39fd866cc06e415abbdbc62ee93f8d7
Thayane Martiniano <thayanemartiniano@gmail.com>
2024-07-04 20:34:15 -0300

    Delete ex005 directory

--- ex005/javaScriptt/quadrado.js
+++
@@ -1,6 +0,0 @@
-"use strict"
-var num1, num2, quadr
-num1=parseFloat(prompt("Digite o primeiro número: "));
-num2=parseFloat(prompt("Digite o segundo número: "));
-quadr=(num1**num2)
-window.alert(num1+"^"+num2+"="+quadr)
